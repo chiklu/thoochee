0cd4211b0e405a50fc4020af4f4a66ac49056566
chiklu <chiklu.ravi@gmail.com>
2013-11-04 23:24:44 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thoochee::Application.config.secret_token = '5c74af0e48d1f6abf7b7eb2c4066d0502d2dfb563899f0f067885e5f5c3e9f82c8d24ae4fb09854acda39b02c05d51cc361ff4589d2b76113b1fdb3c7aef3707'
